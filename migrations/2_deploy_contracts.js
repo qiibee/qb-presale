3051d29b62c406c9197db3702eeeb9675c075ba9
Federico Martín Alconada Verzini <federico.alconada@icloud.com>
2017-10-31 09:30:04 +0100

    change deploy settings

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,24 @@
 const QiibeeCrowdsale = artifacts.require("./QiibeeCrowdsale.sol");
+const QiibeePresale = artifacts.require("./QiibeePresale.sol");
 
 module.exports = function(deployer) {
-  const startTime = web3.eth.getBlock('latest').timestamp // blockchain block number (in timestamp) where the crowdsale will commence.
-  const endTime = startTime + (86400 * 20)  // blockchain block number (in timestamp) where it will end.
-  const rate = new web3.BigNumber(3000) // rate of ether to Qiibee Coin in wei: 3000 qbx are 1 wei
-  const goal = new web3.BigNumber(10000) // minimum amount of qbx to be sold
-  const cap = new web3.BigNumber(10000000000) // max amount of tokens (in atto) to be sold
-  const minInvest = new web3.BigNumber(1) // max amount of tokens (in atto) to be sold
-  const maxInvest = new web3.BigNumber(10000000000) // max amount of tokens (in atto) to be sold
-  const maxGasPrice = new web3.BigNumber(50000000000) // max amount og gas allowed per transaction
-  const maxCallFrequency = new web3.BigNumber(600) // frequency between one call and the next one for an address (in seconds)
+  const startTime = web3.eth.getBlock('latest').timestamp + 300 // blockchain block number (in timestamp) where the crowdsale will commence.
+  // const startTime = 1508930939 + (11 * 60) // blockchain block number (in timestamp) where the crowdsale will commence.
+  const endTime = startTime + 3600  // blockchain block number (in timestamp) where it will end.
+  const rate = new web3.BigNumber(3000) // rate of ether to Qiibee Coin in ether: 3000 qbx are 1 ether
+  const goal = new web3.BigNumber(1500000000000000000000) // minimum amount of qbx to be sold 1500 qbx
+  const cap = new web3.BigNumber(6000000000000000000000) // max amount of tokens (in atto) to be sold 10bn 6000qbx
+  const minInvest = new web3.BigNumber(400000000000000000000) // max amount of tokens (in atto) to be sold 400qbx
+  const maxInvest = new web3.BigNumber(4000000000000000000000) // max amount of tokens (in atto) to be sold 4000qbx
+  const maxGasPrice = new web3.BigNumber(50000000000) // max amount og gas allowed per transaction 50 gwei
+  const maxCallFrequency = new web3.BigNumber(60) // frequency between one call and the next one for an address (in seconds)
+  const presalecap = new web3.BigNumber(10000000000000000000000000000) // max amount of tokens to be sold
+
   const wallet = web3.eth.accounts[0] // the address that will hold the fund. Recommended to use a multisig one for security.
 
-  // deployer.deploy(QiibeeCrowdsale, startTime, endTime, rate, goal, cap, minInvest, maxInvest, maxGasPrice, maxCallFrequency, wallet);
+  // const wallet = "0x9E6C2aD716d6A94f52819aC99acec797FC79015b" // the address that will hold the fund. Recommended to use a multisig one for security.
+    console.log(wallet);
+
+  deployer.deploy(QiibeeCrowdsale, startTime, endTime, rate, goal, cap, minInvest, maxInvest, maxGasPrice, maxCallFrequency, wallet);
+ // deployer.deploy(QiibeePresale, presalecap, wallet);
 };
--- truffle.js
+++ truffle.js
@@ -1,14 +1,25 @@
-var HDWalletProvider = require('truffle-hdwallet-provider');
+var bip39 = require("bip39");
+var ethwallet = require('ethereumjs-wallet');
+var ProviderEngine = require("web3-provider-engine");
+var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
+var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+var Web3 = require("web3");
 
-var mnemonic = '[REDACTED]';
+// Insert raw hex private key here, e.g. using MyEtherWallet
+var wallet = ethwallet.fromPrivateKey(Buffer.from('565ab4d82b78522f88a185c4d533845d66a7eb3bbf1dd7436a4c110527749a72', 'hex'));
+var address = "0x" + wallet.getAddress().toString("hex");
+console.log(address);
+var providerUrl = "https://ropsten.infura.io/leXGApLcGixJvwRW3gCI";
+var engine = new ProviderEngine();
+engine.addProvider(new WalletSubprovider(wallet, {}));
+engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl)));
+// network connectivity error
+engine.on('error', function(err) {
+    // report connectivity errors
+    console.error(err.stack)
+})
 
-if (!process.env.SOLIDITY_COVERAGE){
-  // This is a stub to use in case you begin validating on a testnet using HDWallet.
-  // HDWallet interferes with the coverage runner so it needs to be instantiated conditionally.
-  // For more info see the solidity-coverage FAQ.
-  //
-  // provider = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/')
-}
+engine.start(); // Required by the provider engine.
 
 module.exports = {
   networks: {
@@ -19,17 +30,16 @@ module.exports = {
       gas: 0x5B8D80, // 6000000 gas
       gasPrice: 21000000000 // 21 Gwei
     },
-    coverage: {
-      host: "localhost",
-      port: 8555,
-      network_id: "*",
-      gas: 0xfffffffffff,
-      gasPrice: 0x01
-    },
-    livenet: {
-      host: "10.8.0.6",
-      port: 8545,
-      network_id: "1" // Match any network id
-    },
+    "ropsten": {
+      network_id: 4,    // Use the ID provided at creation time
+      provider: engine, // Use our custom provider
+      from: address,     // Use the address we derived
+      gas: 4700000, // crowdsale uses 4454368 gas
+      gasPrice: 22000000000 // in wei = 22 gwei
+    }
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
   }
 };
