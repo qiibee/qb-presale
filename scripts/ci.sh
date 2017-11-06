57e61eac3ad6cee38c89177fb26a698db796d325
Federico Martín Alconada Verzini <federico.alconada@icloud.com>
2017-11-06 19:35:29 +0100

    Make ci.sh executable

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,6 +6,6 @@ if [ "$SOLIDITY_COVERAGE" = true ]; then
   yarn run coveralls
 else
   yarn lint
-  QB_DEBUG=true yarn test test/token/QiibeeToken.js test/Crowdsale.js test/QiibeeCrowdsale.js test/QiibeePresale.js test/WhitelistedCrowdsale.js
+  QB_DEBUG=true yarn test test/token/QiibeeToken.js test/CreatePresale.js test/CreateCrowdsale.js test/Crowdsale.js test/WhitelistedCrowdsale.js
   QB_DEBUG=true GEN_TESTS_TIMEOUT=400 GEN_TESTS_QTY=40 yarn test test/CrowdsaleGenTest.js test/PresaleGenTest.js
 fi
